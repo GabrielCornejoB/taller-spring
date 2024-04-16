6dc5478cc03cc6d5652ca8f9e66a9abc41da2f65
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-04-16 17:59:16 -0500

    Extra comentary for main application class

--- src/main/java/com/gcornejo/springplayground/SpringPlaygroundApplication.java
+++ src/main/java/com/gcornejo/springplayground/SpringPlaygroundApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPlaygroundApplication  {
 
+    // TODO: Crear el paquete de la capa de application y colocar ese archivo ahí (no me dio el tiempo)
     public static void main(String[] args) {
         SpringApplication.run(SpringPlaygroundApplication.class, args);
     }
